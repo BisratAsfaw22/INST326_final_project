36da144f7307084a6ec8d6076678b2bfe81dfb78
Levis Forbang <lfonsoh@terpmail.umd.edu>
2021-03-31 11:17:50 -0400

    added collaborative_programming_exercise.py

---
+++ collaborative_programming_exercise.py
@@ -0,0 +1,9 @@
+def group_h():
+    """Print interesting information about each member of group H.
+    Side effects:
+        print out information
+    """
+    # About Levis Forbang
+    print(f"Levis Forbang's favorite hobbies: moviegoer, skydiving and traveling.")
+
+group_h()
