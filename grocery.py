4cadadcae400a87d2f7098e1519b36ef2c708b50
anani95 <81641992+anani95@users.noreply.github.com>
2021-04-16 16:10:28 -0400

    Update grocery.py

--- grocery.py
+++ grocery.py
@@ -2,18 +2,7 @@ class Product:
     def __init__(self, name, type, aisle, price, quantity, stock):
 
 class Inventory:
-    """ Generates an inventory by reading through the products CSV file and adding each product to a dictionary, where the product name serves as the key.
-    Attributes:
-        inventory: inventory of all grocery store products
-    """
     def __init__(self, list_of_products):
-        """ Initializes product entries for the inventory object upon instantiation.
-        Args:
-            list_of_products: string value of file path to CSV file with all the products in the grocery store
-        Side Effects:
-            Opens and reads CSV file with product information.
-            Creates and modifies value of tokens and key/value pairs of dictionary of inventory atrribute line by line.
-        """
         *** Generates an inventory by reading through the products CSV file and adding each product to a dictionary, where the product name serves as the key
     def get_product(self, product):
         """ This method has two parameters and it searches for the given product name through the dictionary.
@@ -25,7 +14,12 @@ class Inventory:
 		The product with its information
 	"""
     def suggest(self,product):
-        *** Suggests three products relevant to a designated product according to a specified attribute (type, aisle, etc.)
+	""" This method has two parameter and it is use to recommend three alternate product to the customer.
+	Args: 
+		Product: which will represent the names of the recommend products.
+	Returns: 
+		It should return the name any product that the customer picked from the recommended products.
+	"""
 
 class User:
     def __init__(self,name, cart):
@@ -46,18 +40,21 @@ class User:
 	    query: searches product
 	"""
     def examine(self, product):
-        """ Examines a product and returns its attributes as given in its dictionary entry.
-        Args:
-            product: Product object which the user would like to obtain information from
-        Exceptions:
-            NameError: product is not a product in the inventory
-        Returns:
-            Returns string labels with corresponding variables (name, type, aisle, price, quantity, and stock) as part of the list associated with the product name key.
-        """
+        *** Examines a product and returns its attributes as given in its dictionary entry
 	def add_product_to_cart(self, product, quantity=1):
         *** Adds a specific amount of a particular product to the cart
     def remove_product_from_cart(self, product, quantity=1):
-        *** Removes a specific amount of a particular product from the cart
+	""" This methode has 3 parameters and it is use to a specific amount of a particular product from the customer cart.
+	
+	Args: 
+		product: which represent the list of the product name that the customer has in his cart.
+		
+		Quantity: which represent the exact amount of each product that will be removed from the cart. 
+		
+	Returns:
+		This should return the list of product left in the customer cart after the quantity of product was removed.
+	"""
+        
 	def check_out:
         *** Determines the total cost of the items in the cart
     def print_receipt:
