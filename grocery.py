482a631dc5f7e43fe40caad52421a5073d9e699c
Tyler Perlstein <tylerperlstein@gmail.com>
2021-04-16 23:55:33 -0400

    finalized changes to check-in file

--- grocery.py
+++ grocery.py
@@ -1,28 +1,26 @@
-###class Product:
-    ###def __init__(self, name, type, aisle, price, quantity, stock):
-    
-### CHANGES:
-# Modify docstring length to be shorter than 80 characters per line.
-# Change class/method descriptions to be concise.
-# Include type of variable (str, dict, etc.) when referenced in docstring.
-
 class Inventory:
-	""" Generates an inventory by reading through the products CSV file and adding each product to a dictionary, where the product name serves as the key.
+	""" Generates an inventory.
    	Attributes:
        inventory (dict): inventory of all grocery store products
    	"""
     def __init__(self, list_of_products):
-    	""" Initializes product entries for the inventory object.
+    	""" Creates an inventory by reading through the products CSV file and
+ 				adding each product to a dictionary, where the product name
+     			serves as the key and a dictionary containing the other
+        		attributes servea as the value.
         Args:
-           list_of_products (str): file path to CSV file with all the products in the grocery store
+        	list_of_products (str): file path to CSV file with all grocery store
+           		products
         Side Effects:
-           Creates and modifies value of tokens and key/value pairs of dictionary of inventory atrribute line by line.
+            Creates and modifies values of tokens and key/value pairs of
+           		dictionary of inventory atrribute line by line.
         """
     def get_product(self, product):
-        """ Returns informatoin about the product.
+        """ Returns information about the product.
 	
 	Args:
-	     product (str): represents the product including its name and other information
+	    product (str): represents the product including its name and other
+     		information
 		
 	Returns:
 		A dictionary with the product information
@@ -30,22 +28,24 @@ class Inventory:
     def suggest(self,product):
 	"""recommend three alternate products to the customer.
 	Args: 
-		Product(str): represent the names of the recommend products.
+		Product(str): represent the names of the products.
 	Returns: 
-		The name of any product that the customer picked from the recommended products.
+		The names (str) of three products recommended according to the
+  			attributes of the designated product.
 	"""
 
 class User:
-	""" It asks the user for an input of a product's name of their interest and finds it. Adds product into 
-		the cart or removes unwanted product from the cart.
+	""" It asks the user for an input of a product's name of their interest and
+ 			finds it. Adds product into the cart or removes unwanted product
+    		from the cart.
 
 	Attributes:
 		product(str): indicates information of the product
 		cart(str): contains information on what is in the user's cart
 	""" 
     def __init__(self,name, cart):
-        """ It generates a grocery store application user where it asks the user for an input of a product 
-			name.
+        """ It generates a grocery store application user where it asks the user
+        		for an input of a product name.
 	
 	Args:
 	    name(string): the name of the product
@@ -55,39 +55,44 @@ class User:
 	    Asks the user to input a product of their interest
 	"""
 	def search_product(self, query):
-        """ It looks up a specific product through entry a search term, which then shows ten 
-			results accoording to relevance through Pandas
+        """ It looks up a specific product through entry a search term, which
+        		then shows ten results accoording to relevance through Pandas
 	    
 	Args:
 	    query: finds products
 	"""
-    def examine(self, product):
-    	""" Examines a product and returns its attributes as given in its dictionary entry.
-       	Args:
-           product: Product object which the user would like to obtain information from
-      	Exceptions:
-           NameError: product is not a product in the inventory
-      	Returns:
-           Returns string labels with corresponding variables (name, type, aisle, price, quantity, and stock) as part of the list associated with the product name key.
-        """
 	def add_product_to_cart(self, product, quantity=1):
-        *** Adds a specific amount of a particular product to the cart
+        """ Adds a specific amount of a particular product to the customer cart.
+        Args: 
+			product (str): represent the name key value of the product that the
+   				customer has in the cart.
+   			quantity (int): represents the exact amount of each product that
+      			will be added to the cart. 
+      	Returns:
+			Returns the quantity of product in the customer cart after the
+   				specified quantity of product was removed.
+		"""
     def remove_product_from_cart(self, product, quantity=1):
 	""" Remove a specific amount of a particular product from the customer cart.
 	
 	Args: 
-		product(str): represent the list of the product name that the customer has in his cart.
+		product(str): represent the list of the product name that the customer
+  			has in his cart.
 		
-		Quantity(int): represent the exact amount of each product that will be removed from the cart. 
+		Quantity(int): represent the exact amount of each product that will be
+  			removed from the cart. 
 		
 	Returns:
-		returns the list of product left in the customer cart after the quantity of product was removed.
+		returns the list of product left in the customer cart after the quantity
+  			of product was removed.
 	"""
-        
-	def check_out:
-        *** Determines the total cost of the items in the cart
-    def print_receipt:
-        *** Prints out a list of the items using Pandas, along with the total cost
-
+ 
+	###def check_out:
+        # Determines the total cost of the items in the cart
+    ###def print_receipt:
+        # Prints out a list of the items using Pandas, along with the total cost
 if __name__ == "__main__":
+    """ Runs specific order of function calls when program is operated (will
+    		be more fleshed out later on).
+    """
     args = parse_args(sys.argv[1:])
