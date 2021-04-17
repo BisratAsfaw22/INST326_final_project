0d040835a93ada92b0167db9ee5a2664007cf916
Tyler Perlstein <tylerperlstein@gmail.com>
2021-04-17 02:07:00 -0400

    finalized changes to check-in file

--- grocery.py
+++ grocery.py
@@ -7,92 +7,106 @@ class Inventory:
     	""" Creates an inventory by reading through the products CSV file and
  				adding each product to a dictionary, where the product name
      			serves as the key and a dictionary containing the other
-        		attributes servea as the value.
+        		characteristics serves as the value.
         Args:
         	list_of_products (str): file path to CSV file with all grocery store
            		products
         Side Effects:
             Creates and modifies values of tokens and key/value pairs of
-           		dictionary of inventory atrribute line by line.
+           		dictionary for inventory atrribute line by line.
         """
-    def get_product(self, product):
+    def get_information(self, product):
         """ Returns information about the product.
-	
-	Args:
-	    product (str): represents the product including its name and other
-     		information
-		
-	Returns:
-		A dictionary with the product information
-	"""
-    def suggest(self,product):
-	"""recommend three alternate products to the customer.
-	Args: 
-		Product(str): represent the names of the products.
-	Returns: 
-		The names (str) of three products recommended according to the
-  			attributes of the designated product.
-	"""
+		Args:
+	    	product (str): name (dictionary key) of product
+		Returns:
+			characteristics (dict): dictionary value associated with name key
+   				containing rest of product information (i.e. price, aisle).
+		"""
+    def suggest(self, product):
+		""" Recommends three alternative products to the customer.
+		Args: 
+			product (str): name (dictionary key) of product
+		Returns: 
+			results (list): list of names (strings) of three alternative
+   				products recommended according to the characteristics of the
+     			specified product.
+		"""
 
 class User:
-	""" It asks the user for an input of a product's name of their interest and
- 			finds it. Adds product into the cart or removes unwanted product
-    		from the cart.
-
+	""" Generates a user.
 	Attributes:
-		product(str): indicates information of the product
-		cart(str): contains information on what is in the user's cart
+ 		name (str): name of user
+		cart (dict): container which stores products
 	""" 
-    def __init__(self,name, cart):
-        """ It generates a grocery store application user where it asks the user
-        		for an input of a product name.
-	
-	Args:
-	    name(string): the name of the product
-	    cart: represents the user's cart and includes the products in the cart
-	    
-	Returns:
-	    Asks the user to input a product of their interest
-	"""
+    def __init__(self, name, cart):
+        """ Generates a grocery store application user who can search for a
+        		product, as well as add products to a cart or remove unwanted
+          		products from the cart. When finished, the user can check out.
+        Args:
+	    	name (str): name of user
+	    	cart (dict): container which stores product names (strings) as
+      			keys and quantities (integers) as associated values
+        Side Effects:
+            Modifies values of name and cart.
+    	"""   
 	def search_product(self, query):
-        """ It looks up a specific product through entry a search term, which
-        		then shows ten results accoording to relevance through Pandas
-	    
-	Args:
-	    query: finds products
-	"""
-	def add_product_to_cart(self, product, quantity=1):
-        """ Adds a specific amount of a particular product to the customer cart.
-        Args: 
-			product (str): represent the name key value of the product that the
-   				customer has in the cart.
-   			quantity (int): represents the exact amount of each product that
-      			will be added to the cart. 
-      	Returns:
-			Returns the quantity of product in the customer cart after the
-   				specified quantity of product was removed.
+        """ Looks up a specific product through entry of a search term, which
+        		then shows ten results accoording to relevance through Pandas.
+	    Args:
+     		query (str): search term
+		Side Effects:
+			Modifies values of query and results.
+		Returns:
+			results (list): list of ten product names (strings) which match the
+   				query
 		"""
-    def remove_product_from_cart(self, product, quantity=1):
-	""" Remove a specific amount of a particular product from the customer cart.
-	
-	Args: 
-		product(str): represent the list of the product name that the customer
-  			has in his cart.
-		
-		Quantity(int): represent the exact amount of each product that will be
-  			removed from the cart. 
-		
-	Returns:
-		returns the list of product left in the customer cart after the quantity
-  			of product was removed.
-	"""
- 
-	###def check_out:
-        # Determines the total cost of the items in the cart
-    ###def print_receipt:
-        # Prints out a list of the items using Pandas, along with the total cost
+	def add_product_to_cart(self, product, amount=1):
+        """ Adds a specific amount of a particular product to the cart.
+        Args: 
+			product (str): name (dictionary key) of product
+   			amount (int): exact amount of product that will be added to the
+      			cart
+        Side Effects:
+			Modifies values of quantity and stock key/value pairs of
+           		dictionary for cart attribute.
+        Raises:
+			ValueError: amount is less than 1.
+      	Returns:
+			cart_status (str): statement of the quantity (integer) of product in
+   				the customer cart after the specified amount of product was
+       			added.
+		"""
+    def remove_product_from_cart(self, product, amount=1):
+		""" Removes a specific amount of a particular product from the cart.
+  		Args: 
+			product (str): name (dictionary key) of product
+     		amount (int): exact amount of product that will be added to the
+      			cart
+        Side Effects:
+			Modifies values of quantity and stock key/value pairs of
+           		dictionary for cart attribute.
+        Raises:
+			ValueError: amount is less than 1.
+     	Returns:
+			cart_status (str): statement of the quantity (integer) of product in
+   				the customer cart after the specified amount of product was
+       			removed.
+		"""
+    def print_receipt(self):
+        """ Prints statement of all products, amounts, and total cost using
+        		Pandas.
+        Side Effects:
+			Prints dictionary of all product names and associated quantities
+   				as part of cart dictionary.
+   			Prints total cost (float) of all products.
+		"""
+    ###def check_out:
+    	# Determines the total cost of the products in the cart
 if __name__ == "__main__":
     """ Runs specific order of function calls when program is operated (will
     		be more fleshed out later on).
     """
-    args = parse_args(sys.argv[1:])
+    User.name = input("What is your name?")
+    print ("Welcome, " + User.name + " .")
+    User.search_product("What are you looking for?")
