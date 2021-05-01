da7dd9696bf7e340d9a14d2faba9bdc2fd0b218d
Bisrat Asfaw <basfaw@umd.edu>
2021-04-30 21:21:21 -0400

    updated

--- grocery.py
+++ grocery.py
@@ -1,93 +1,151 @@
-###class Product:
-    ###def __init__(self, name, type, aisle, price, quantity, stock):
+
+import csv
+
+class IventoryClass:
+    """ This is the main class of the program having the following attributes:
     
-### CHANGES:
-# Modify docstring length to be shorter than 80 characters per line.
-# Change class/method descriptions to be concise.
-# Include type of variable (str, dict, etc.) when referenced in docstring.
-
-class Inventory:
-	""" Generates an inventory by reading through the products CSV file and adding each product to a dictionary, where the product name serves as the key.
-   	Attributes:
-       inventory (dict): inventory of all grocery store products
-   	"""
-    def __init__(self, list_of_products):
-    	""" Initializes product entries for the inventory object.
-        Args:
-           list_of_products (str): file path to CSV file with all the products in the grocery store
-        Side Effects:
-           Creates and modifies value of tokens and key/value pairs of dictionary of inventory atrribute line by line.
-        """
-    def get_product(self, product):
-        """ Returns informatoin about the product.
-	
-	Args:
-	     product (str): represents the product including its name and other information
-		
-	Returns:
-		A dictionary with the product information
-	"""
-    def suggest(self,product):
-	""" This method has two parameter and it is use to recommend three alternate product to the customer.
-	Args: 
-		Product: which will represent the names of the recommend products.
-	Returns: 
-		It should return the name any product that the customer picked from the recommended products.
-	"""
-
-class User:
-	""" It asks the user for an input of a product's name of their interest and finds it. Adds product into 
-		the cart or removes unwanted product from the cart.
-
-	Attributes:
-		product(str): indicates information of the product
-		cart(str): contains information on what is in the user's cart
-	""" 
-    def __init__(self,name, cart):
-        """ It generates a grocery store application user where it asks the user for an input of a product 
-			name.
-	
-	Args:
-	    name(string): the name of the product
-	    cart: represents the user's cart and includes the products in the cart
-	    
-	Returns:
-	    Asks the user to input a product of their interest
-	"""
-	def search_product(self, query):
-        """ It looks up a specific product through entry a search term, which then shows ten 
-			results accoording to relevance through Pandas
-	    
-	Args:
-	    query: finds products
-	"""
-    def examine(self, product):
-    	""" Examines a product and returns its attributes as given in its dictionary entry.
-       	Args:
-           product: Product object which the user would like to obtain information from
-      	Exceptions:
-           NameError: product is not a product in the inventory
-      	Returns:
-           Returns string labels with corresponding variables (name, type, aisle, price, quantity, and stock) as part of the list associated with the product name key.
-        """
-	def add_product_to_cart(self, product, quantity=1):
-        *** Adds a specific amount of a particular product to the cart
-    def remove_product_from_cart(self, product, quantity=1):
-	""" This methode has 3 parameters and it is use to a specific amount of a particular product from the customer cart.
-	
-	Args: 
-		product: which represent the list of the product name that the customer has in his cart.
-		
-		Quantity: which represent the exact amount of each product that will be removed from the cart. 
-		
-	Returns:
-		This should return the list of product left in the customer cart after the quantity of product was removed.
-	"""
+    Attributes:
+        inventory_dictionary (dictionary): values are list of tuples
+        found (list): list of tuples
+        not_found (list): list of tuples
+        t_amount (float): Total amount
+        t_qty (int): total quantity
+    """
         
-	def check_out:
-        *** Determines the total cost of the items in the cart
-    def print_receipt:
-        *** Prints out a list of the items using Pandas, along with the total cost
+    
+    def __init__(self, inventory_dictionary=dict(), found=list(), not_found=list(), t_amount=0.0, t_qty=0):
+        """ This method establish the following attributes of the class:
+        
+        Attributes:
+            
+            inventory_dictionary (dictionary): values are list of tuples
+            found (list): list of tuples
+            not_found (list): list of tuples
+            t_amount (float): Total amount
+        """
+        
+        self.inventory_dictionary = inventory_dictionary
+        self.found = found 
+        self.not_found = not_found
+        self.t_amount = t_amount
+        self.t_qty = t_qty
+        
+    
+    def UpdateInventory(self, file1):
+        """ This method reads and updates the inventory_dictionary attribute
+        with data from the csv file. 
+        
+        Args:
+            file1 (str): csv
+        
+        Side effects:
+            update the inventory_dictionary attribute
+        """
+        
+        t = open(file1, 'r')
+        reader = csv.reader(t)
+        
+        for row in reader:
+            self.inventory_dictionary.update(
+                                                {row[0]:[('Food Category', row[1]), 
+                                                         ('Aisle Number', row[2]), 
+                                                         ('Price', row[3]), 
+                                                         ('On Sale', row[4]), 
+                                                         ('QTY On Shelf', row[5]), 
+                                                         ('In Stock', row[6])
+                                                        ]
+                                                 }
+                                            )
+     
+    def UpdateLists(self, file2):
+        """ This method reads a text file and updates
+        "found" and "not_found" attributes accordingly
+        
+        Args:
+            file2 (str): text file
+        
+        Side effects:
+            Update "found" and "not_found" attributes
+        """
+            
+        #print(self.inventory_dictionary)
+        with open(file2, "r", encoding="utf-8") as f:
+            for line in f:
+                parts = line.strip().split("\t")
+                parts2 = parts[0], parts[1]
+                #print(parts2)
+                if parts[0] in self.inventory_dictionary:
+                    ##print(parts2)
+                    self.found.append(parts2)
+                else:
+                    self.not_found.append(parts2)
+        
+        
+    def TotalCalc(self):
+        """ This method updates the t_amount attribute accordingly.
+        
+        Side effects:
+            Update the t_amount attribute
+            update the t_qty attribute
+        
+        Returns:
+            Returns the t_amount and t_qty attributes as tuples 
+        """
+        
+        print(f'The following are the item(s) on your list that was found.') # For testing
+        for item in self.found:
+            
+            item_key = item[0]
+            item_qty = int(item[1])
+            self.t_qty += item_qty
+            hj = self.inventory_dictionary.get(item_key)
+            print(f'Item name: {item_key}') # for testing
+            print(f'Informatin about the item: {hj}') # for testing
+            for q in hj:
+                if q[0] == 'Price':
+                    self.t_amount += float(q[1]) * float(item_qty)
+        return f'Total cost: ${self.t_amount}, Total qty: {self.t_qty}' # "Total cost: $" & Total qty:" will be removed
+        
+    def Totalitem(self):
+        """It gives the total items on the users list and the total items that were found.
+        
+            
+            Side effects:
+                Prints to the console  
+        """
+        list_file = open('sample_items.txt', "r")
+        count = 0
+        list1 = list_file.read()
+        shopping_list = list1.split("\n")
+        for i in shopping_list:
+            if i:
+                count += 1
+        print (f'Total item(s) on your list: {count}')
+        i in self.found
+        return f'Total item(s) of your list found in the store: {count-1}'
+                    
+## Replace this comment with method(s) that display the following information
+        #   Total items(s) not fund: 1
+                # Display the names(key) of the item(s)
+                    # This should be in a constructive sentence
+        #   Total coast including inclusding tax:
+                # This should be in a constructive sentence
+        
 
-if __name__ == "__main__":
-    args = parse_args(sys.argv[1:])
+## Replace this comment with a method(s) that displace the following information
+
+        # Display items that are on sale based on items note found
+            # It can be item(s) on sale that share similar key term in name (eg. eggs)
+            
+            
+## Replace This comment with folliwng information 
+
+        # an if __name__ == "__main__": statement and the proceeding information
+    
+p = IventoryClass() # for testing
+k = p.UpdateInventory('inst326_project.csv') # for testing
+n = p.UpdateLists('sample_items.txt') # for testing
+ll = p.TotalCalc() # for testing
+t = p.Totalitem() # for testing
+print(t) # for testing
+print(ll) # for testing
